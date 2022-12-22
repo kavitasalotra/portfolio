289ada0b426120e15998d6976d1ca685f4c14ae4
dhairyasharma504 <dhairyasharma2311@gmail.com>
2022-12-22 18:42:25 +0530

    fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-<nav class="bg-gray-800">
+<nav class=" bg-brand">
   <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div class="relative flex h-16 items-center justify-between">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../src/components/Navbar'
+import Navbar from '../components/Navbar'
 
 const index = () => {
   return (
